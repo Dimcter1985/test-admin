d9188fea86b8d259e569380b5d5a0c0459de5a8a
Dmitriy Rybnikov <senior.ribnickow1985@yandex.ru>
2021-03-25 09:14:47 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,31 @@ import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import authProvider from './authProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark', // Switching the dark mode on is a single property value change.
+  },
+  overrides: {
+    MuiAppBar: {
+      colorSecondary: {
+        backgroundColor: 'blue'
+      }
+    }
+  }
+});
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => {
 
-  return <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} >
+  return <Admin 
+          title='Панель администратора' 
+          dashboard={Dashboard} 
+          authProvider={authProvider} 
+          dataProvider={dataProvider} 
+          theme={theme}
+          >
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
